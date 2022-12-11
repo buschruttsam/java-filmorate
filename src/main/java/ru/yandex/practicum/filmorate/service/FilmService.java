80ea3cade41f78b809d392b2388156fcf273793d
buschruttsam <buschrutt@gmail.com>
2022-12-10 22:42:57 -0600

    inMemoryFilm logic created

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -1,7 +1,14 @@
 package ru.yandex.practicum.filmorate.service;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.domain.exeptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
 
+import java.time.LocalDate;
+
+@Component
 @Service
 public class FilmService {
+
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -1,4 +1,16 @@
 package ru.yandex.practicum.filmorate.storage;
 
+import ru.yandex.practicum.filmorate.domain.exeptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
 public interface FilmStorage {
+
+    Film create(Film film) throws ValidationException;
+
+    Film update(Film film) throws ValidationException;
+
+    int updateLastFilmId();
+
+    void filmValidation(Film film) throws ValidationException;
+
 }
